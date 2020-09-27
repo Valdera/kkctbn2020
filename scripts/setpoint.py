287639f68e661dd82023bb66bc7a82251d2b1394
Fauzan Valdera <f.valdera@yahoo.co.id>
2020-09-27 13:27:00 +0700

    Update setpoint.py

--- scripts/setpoint.py
+++ scripts/setpoint.py
@@ -2,36 +2,24 @@
 # If using ROS Noetic on Ubuntu 20.04 change python to python3 to make it working
 import rospy
 from std_msgs.msg import Float64
+from kkctbn2020.msg import AutoControl
 
-def setpoint():
-    # initialize the node
-    rospy.init_node('setpoint', anonymous=True)
-    # set setpoint as ROS Publisher
-    setpoint_node = rospy.Publisher("setpoint", Float64, queue_size=1)
 
-    rospy.loginfo("Starting setpoint publisher")
-
+def autoControlCallback(msg):
     # Check if ROS Publisher is still active
-    # Check the time
-    while ((not rospy.is_shutdown()) and (rospy.Time(0) == rospy.Time.now())):
-        rospy.loginfo("setpoint_node spinning, waiting for time to become non-zero")
-        rospy.sleep(1)
-
-    # setpoint -10 as Float64 
-    setpoint = -10
-    # Set the rate to 0.2 Hz which is 5 seconds
-    rate = rospy.Rate(0.2)
-
-    # Publish setpoint if ROS is still active
-    while not rospy.is_shutdown():
-        # Publish setpoint value
-        setpoint_node.publish(setpoint)
-        rate.sleep()
+    if (msg.state == AutoControl.AVOID_RED_AND_GREEN):
+        setpoint = 640 / 2
+    else {
+        setpoint = -10
+    }
+    
+    setpoint_node.publish(setpoint)
 
 
 if __name__ == '__main__':
     # If ROS isn't active, do nothing
-    try:
-        setpoint()
-    except rospy.ROSInterruptException:
-        pass
+    rospy.init_node('setpoint', anonymous=True)
+    setpoint_node = rospy.Publisher("setpoint", Float64, queue_size=1)
+    auto_control_subscriber = rospy.Subscriber("/makarax/auto_control", AutoControl, autoControlCallback)
+
+    
